6ad300ad5d0a08c9bc4f3aa1e033a3b37923e03a
Lora Vernon-Kidd <lav4@hotmail.com>
2014-09-29 05:13:40 -0400

    Save 24

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WinesReview2::Application.config.secret_key_base = 'b8ee204495da6c67a5d64cfd7071a6281b2a1c3d08e40ab3c894029fa34f24d84f568e54ba3e984ee823cb3505458495338e34b731cd26923d1272e5861fa037'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WinesReview2::Application.config.session_store :cookie_store, key: '_wines_review_2_session'
