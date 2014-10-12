d4fc21b6e25bcf4e72a72644528fdac0f08c5f7c
Lora Vernon-Kidd <lav4@hotmail.com>
2014-10-12 16:12:30 -0400

    Save 27

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,8 +1,14 @@
 class WinesController < ApplicationController
+
   def index
   	@available_at = Time.now
   	@wines = Wine.all
   end
+
+  def show
+  	@wines = Wine.find(params[:id])
+  end
+
 end
 
 
