22c0989906fa2b8b5aa048292d7b19df3321c1a7
Lora Vernon-Kidd <lav4@hotmail.com>
2014-10-13 02:04:54 -0400

    Class 32 -- for real

---
+++ test/controllers/wines_controller_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class WinesControllerTest < ActionController::TestCase
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:wines)
+  end
+
+  test "should get show" do
+    get :show, id: wines(:Lakeridge).id
+    assert_response :success
+    assert_not_nil assigns(:wine)
+  end
+
+end
+
