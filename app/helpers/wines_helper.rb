e56cc0b780a0f9c2372234536d2972c375507982
Lora Vernon-Kidd <lav4@hotmail.com>
2014-10-13 11:23:18 -0400

    Save 53

--- app/helpers/wines_helper.rb
+++ app/helpers/wines_helper.rb
@@ -1,6 +1,17 @@
 module WinesHelper
+
   def formatted_time(time)
     time.strftime("%B %d, %Y at %l:%M %P")
   end
+
+  def format_average_stars(wine)
+  	average = wine.average_stars
+  	if average
+  	  pluralize(number_with_precision(average, precision: 1), 'star')
+  	else
+  	  'No Reviews'
+  	end
+ end
+
 end
 
