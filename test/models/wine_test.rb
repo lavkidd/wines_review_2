73799c9ccbddb9774f33b247ea6a510b87d8752c
Lora Vernon-Kidd <lav4@hotmail.com>
2014-10-12 20:24:43 -0400

    Save 31a -- 2nd time

--- test/models/wine_test.rb
+++ test/models/wine_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class WineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+  	@wine = Wine.new
+  end
+
+  test 'name remembers its value' do
+    @wine.name = 'Famiglia Bertona'
+    assert_equal 'Famiglia Bertona', @wine.name
+  end
+
+  test 'year remembers its value' do
+    @wine.year = 2008
+    assert_equal 2008, @wine.year
+  end
+
+  test 'winery remembers its value' do
+    @wine.winery = 'Famiglia Bertona'
+    assert_equal 'Famiglia Bertona', @wine.winery
+  end
+
+  test 'country remembers its value' do
+  	@wine.country = 'Italy'
+  	assert_equal 'Italy', @wine.country
+  end
+
+  test 'varietal remembers its value' do
+  	@wine.varietal = 'Malbec'
+  	assert_equal 'Malbec', @wine.varietal
+  end
+
+  test persistence works properly do
+    created_wine = wine.create(name:     'Colleziione di Paola',
+    	                       year:      2012,
+    	                       winery:   'Colleziione di Paola',
+    	                       country:  'Italy'
+    	                       varietal: 'Sangiovese')
+    found_wine = Wine.fine created_wine.id
+    assert_not_nil found_wine
+    assert_equal 'Colleziione di Paola', found_wine.name
+    assert_equal 2012, found_book.year
+    assert_equal 'Colleziione di Paola', found_wine.winery
+    assert_equal 'Italy', found_wine.country
+    assert_equal 'Sangiovese', found_wine.varietal
+  end
+
 end
+
