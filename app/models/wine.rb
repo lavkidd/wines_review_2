54bf50d6704501a84aeaef77654d16f905488b6c
Lora Vernon-Kidd <lav4@hotmail.com>
2014-10-13 09:58:07 -0400

    Save 45

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,6 +1,9 @@
 class Wine < ActiveRecord::Base
 
-validates :name, :year, :winery, :country, :varietal, presence: true
+validates :name, :presence => true, allow_blank: false
+
+validates :year, :winery, :country, :varietal, presence: true
+
 validates :year,
   numericality: { only_integer: true, greater_than_or_equal_to: 2000 },
   unless: "year.blank?"
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ WinesReview2::Application.routes.draw do
 #  delete "/wines/:id"      => "wines#destroy"
 
 resources :wines
+root 'wines#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
