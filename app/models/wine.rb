259e790d6be8dbefb9e762fdc90c6fcb91a5be03
Lora Vernon-Kidd <lav4@hotmail.com>
2014-10-13 12:02:13 -0400

    Save 55

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,14 +1,18 @@
 class Wine < ActiveRecord::Base
 
-validates :name, :presence => true, allow_blank: false
+  validates :name, :presence => true, allow_blank: false
 
-validates :year, :winery, :country, :varietal, presence: true
+  validates :year, :winery, :country, :varietal, presence: true
 
-validates :year,
-  numericality: { only_integer: true, greater_than_or_equal_to: 2000 },
-  unless: "year.blank?"
+  validates :year,
+    numericality: { only_integer: true, greater_than_or_equal_to: 2000 },
+    unless: "year.blank?"
 
-has_many :reviews
+  has_many :reviews, dependent: :destroy
+
+  def average_stars
+  	reviews.average(:stars)
+  end
 
 end
 
